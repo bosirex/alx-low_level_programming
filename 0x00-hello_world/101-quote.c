0beed3bbe6a1c148794bf67c66145bc77b648428
bosirex <dennis.dancan@gmail.com>
2023-02-16 10:21:50 -0800

    my commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,7 @@
 #include <unistd.h>
 
  /**
- *main - prints exactly and that piece of art is useful" - Dora Korpar,
- *2015-10-19, followed by a new line, to the standard error.
+ *main - Entry point
  *
  *Return: 1 (Success)
  */
