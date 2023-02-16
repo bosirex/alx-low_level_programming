68b97fe74e30b7922647a936d7f73644214cc34b
bosirex <dennis.dancan@gmail.com>
2023-02-16 10:02:05 -0800

    my commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <stdlib.h>
 #include <stdlib.h>
 /**
  *main -prints exactly and that piece of art is useful" - Dora Korpar,
@@ -8,7 +8,7 @@
  */
 int main(void)
 {
-	write(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2\015-10-19\n", 59);
-
+	write(STDOUT_FILENO, "and that piece of art is useful
+			\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
