ab1dc3ccdbf8fbb0aac657d5768b5538f80330ae
bosirex <dennis.dancan@gmail.com>
2023-02-17 04:52:01 -0800

    my commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints all single digit numbers of base 10
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	char j;
+
+	for (j = 0; j < 10; j++)
+		putchar(j);
+
+
+	putchar('\n');
+
+	return (0);
+}
