9c942f9990ecd652048d755f7722c45d77204b01
bosirex <dennis.dancan@gmail.com>
2023-02-21 03:59:09 -0800

    my commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Make alphabet x10 times
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+
+{
+
+	char letter;
+	int j = 0;
+
+	while (j <= 9)
+	{
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+	_putchar(letter);
+	}
+	_putchar('\n');
+	j++;
+	}
+}
