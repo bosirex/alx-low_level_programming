b67591925c7fc02764b6733952d2b31bbc05e142
bosirex <dennis.dancan@gmail.com>
2023-02-27 02:09:56 -0800

    my commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - function that takes a pointer to an int as parameter and updates
+ * the value
+ * Return: nothing.
+ */
+void reset_to_98(int *n);
+{
+	*n = 98;
+}
