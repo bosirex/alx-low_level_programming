587c744f3e68c53df1bc8b2ea5cf7596067c5924
bosirex <dennis.dancan@gmail.com>
2023-02-27 05:18:36 -0800

    my commit

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * print_rev - prints a string, in reverse, followed by a new line
+ *
+ * @s: string to be printed
+ *
+ * Return: nothing
+ */
+void print_rev(char *s)
+{
+       int ch = 0;
+
+	while (s[ch] != '\0')
+	{
+	c++;
+	}
+
+	for (ch -= 1; ch >= 0; ch--)
+	{
+	putchar(s[ch]);
+	}
+	putchar('\n');
+}
