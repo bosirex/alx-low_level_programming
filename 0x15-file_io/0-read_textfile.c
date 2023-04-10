57aed1cc908deb766a2b17b4be7c5de85579076d
bosirex <dennis.dancan@gmail.com>
2023-04-10 07:51:23 -0700

    file handling

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -3,8 +3,8 @@
 
 /**
  * read_textfile- Read text file print to STDOUT.
- * @filename: text file being read
- * @letters: number of letters to be read
+ * @filename: read text file
+ * @letters: number of letters
  * Return: w- actual number of bytes read and printed
  *        0 when function fails or filename is NULL.
  */
